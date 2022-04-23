bab1645df263d961c61ed3f7514806fec00ff7d4
sa1583 <sa1583@naver.com>
2022-04-23 21:47:44 +0900

    ADD: 위젯 추가

---
+++ app/src/main/java/fastcampus/aop/part2/myfinedust/appwidget/SimpleAirQualityWidgetProvider.kt
@@ -0,0 +1,15 @@
+package fastcampus.aop.part2.myfinedust.appwidget
+
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+
+class SimpleAirQualityWidgetProvider: AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context?,
+        appWidgetManager: AppWidgetManager?,
+        appWidgetIds: IntArray?
+    ) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+    }
+}
